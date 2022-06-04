eb455a27a915b54fc300b0415c812e4c3a937898
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-03 19:28:12 -0700

    first commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char d = '0';
+
+	while (d <= '9')
+{
+		putchar(d);
+		d++;
+}
+
+	char c = 'a';
+
+	while (c <= 'f')
+{
+		putchar(c);
+		c++;
+}
+
+	putchar('\n');
+	return (0);
+}
