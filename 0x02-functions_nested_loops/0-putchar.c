3773c85afb9a004ae436433450de823a1af09a0c
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-07 17:01:23 -0700

    Betty style fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * main - prints _putchar
+ *
+ * Description: prints holberton
+ * Return: returns 0
+ */
 int main(void)
 {
 	char str[] = "_putchar\n";
 	int i = 0;
-	while(i < 10)
+
+	while (i < 10)
 	{
 	_putchar(str[i]);
 	i++;
