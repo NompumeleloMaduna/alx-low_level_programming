c968e54c59354004adef75d919ad67bd08887b7d
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-07 19:23:45 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - prints sign of a number
+ * @n: number checked
+ *
+ * Return: 1 and + if n > 0, 0 if n = 0 and -1 and - if n<0
+ */
+int print_sign(int n)
+{
+if( n > 0)
+{
+return (1);
+_putchar(43);
+}
+else if (n < 0)
+{
+return (0);
+_putchar(48);
+}
+else if (n < 0)
+{
+return (-1);
+_putchar(45);
+}
+}
