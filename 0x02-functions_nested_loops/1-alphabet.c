4b004986347ea6de409dc3675be91529b230eb59
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-07 17:24:11 -0700

    printing alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Description: prints the alphabet in lowercase followed by a new line
+ * Return: returns 0
+ */
 void print_alphabet(void)
 {
 char a;
