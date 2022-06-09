121c0f218cf01c5e2ded1bb825be1287bf4d107f
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-09 14:22:41 -0700

    multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*postitive_or_negative() - prints postive and negative integers
+*
+* Description - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+void postitive_or_negative()
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
