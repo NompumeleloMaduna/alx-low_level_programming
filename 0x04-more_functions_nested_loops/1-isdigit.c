2928fa51ec293392ed10a9998089a238cf0263f5
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-10 15:07:58 -0700

    indentation

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,12 +6,11 @@
  * Description: return 1 if c is a digit and 0 otherwise 
  * Return: 0
  */
-
 int _isdigit(int c)
 {
-if (c >= 47 && c <= 57)
-{
-return (1);
-}
-return (0);
+	if (c >= '0' && c <= '9')
+
+		return (1);
+	else
+		return (0);
 }
