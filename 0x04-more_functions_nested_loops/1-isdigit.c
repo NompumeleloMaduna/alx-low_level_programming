bd21d2842aae3f9b60909efd688c488462a230bc
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-10 14:58:19 -0700

    first commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,15 @@
 /**
  * int _isdigit(int c) - returns 1 if c is a digit
  * @c: character that is checked
- * Description: returns 1 if c is a digit or 0 otherwise
+ * 
  * Return: 0
  */
+
 int _isdigit(int c)
 {
-	if (c >= 47 && c <= 57)
-		return (1);
-	return (0);
+if (c >= 47 && c <= 57)
+{
+return (1);
+}
+return (0);
 }
