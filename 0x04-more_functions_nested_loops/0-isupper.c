ea6b01e4444758f3d30a856be98527cf2c9136ca
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-10 14:30:31 -0700

    trailing white space

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,15 +3,13 @@
 /**
 * i_supper - checks for uppercase
 * @c: character being checked
-* Description: returns 1 if c is uppercase 
+* Description: returns 1 if c is uppercase
 * Return: 0
 */
 int _isupper(int c)
 {
-if (c >= 65 && c <= 90)
-{
-return (1);
-}
+	if (c >= 65 && c <= 90)
+	return (1);
 
 return (0);
 }
