1b52fe41e0cb722a7768cd1c6847e85bb26c3add
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-10 13:49:59 -0700

    first attempt

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * i_supper - checks for uppercase
+ * @c: character being checked
+ * Description: returns 1 if c is uppercase 
+ * Return: 0
+ */
+
+int _isupper(int c)
+{
+if (c == 'C')
+{
+	return (1);
+}
+	return (0);
+}
