12ae48d72fde3b0fe26d5dc54eaecb9975c7a065
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-10 15:13:16 -0700

    indentation

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * int _isdigit(int c) - returns 1 if c is a digit
+ * int _isdigit(int c) - checks if c is a digit
  * @c: character that is checked
- * Description: return 1 if c is a digit and 0 otherwise 
- * Return: 0
+ *  
+ * Return: return 1 if c is a digit and 0 otherwise
  */
 int _isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
+	if (c >= 48 && c <= 57)
+	{
 
 		return (1);
-	else
+	}
 		return (0);
 }
