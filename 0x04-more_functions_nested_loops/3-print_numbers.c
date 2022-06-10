6be94be1729a78ff8ded0add743c268642eb04da
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-10 16:09:29 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers() - prints numbers 0 - 9
+ * Description: prints numbers 0 to 9 followed by a new line
+ * Return: 0 - 9
+ */
+void print_numbers(void)
+{
+	int i;
+
+	if (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
