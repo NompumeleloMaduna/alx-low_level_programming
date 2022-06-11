7bdb60a38aa5592e74d077eb4c628dd376daa5bb
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-10 19:52:10 -0700

    betty

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * print_numbers() - prints numbers 0 - 9
+ * print_numbers - prints numbers 0 - 9
  * Description: prints numbers 0 to 9 followed by a new line
+ * Return: 0 to 9
  */
 void print_numbers(void)
 {
