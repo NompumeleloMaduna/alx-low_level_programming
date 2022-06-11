6701e7f3ebff189ff5b8576f862fe3529fefd7e0
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-10 19:41:24 -0700

    betty doc

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,18 @@
 #include "main.h"
-
 /**
-* _i_supper - checks for uppercase
-* @c: character being checked
-* Description: returns 1 if c is uppercase
-* Return: 0
-*/
+ *_isupper - checks for uppercase characters
+ *@c:character checked
+ *Return:1 - uppercase,0 - otherwise
+ *
+ */
 int _isupper(int c)
 {
-if (c >= 65 && c <= 90)
-{
-return (1);
-}
-return (0);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
