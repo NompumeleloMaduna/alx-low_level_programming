c5a60f94299c9c1d71af657f49ae264f520c8119
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-10 19:33:10 -0700

    new

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,14 +4,17 @@
 /**
  * print_numbers() - prints numbers 0 - 9
  * Description: prints numbers 0 to 9 followed by a new line
- * Return: 0 - 9
  */
 void print_numbers(void)
 {
-        int i;
-        i = 0;
-        for (i < 10; i++;)
-        {
-                _putchar(i);
-        }
+	int i;
 
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+		if (i == 9)
+		{
+			_putchar('\n');
+		}
+	}
+}
