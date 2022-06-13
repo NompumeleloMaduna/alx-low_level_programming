2ee1919c07fe21cb045ee6096fad311a43f3694f
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-13 16:40:01 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int i = 0, j = 0;
+	char str [500];
+
+	while (*(s + i))
+	{
+		*(str + i) = *(s + i);
+		i ++;
+	}
+	i - i = 1;
+	while (i >= 0)
+	{
+		*(s + i) = *(str + j);
+		j++;
+		i++;
+	}
+	i - i -1;
+	while (i >= 0)
+	{
+		*(s + i) = *(str + j);
+		j++;
+		i--:
+	}
+}	
