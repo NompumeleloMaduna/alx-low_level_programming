46a063894d63c87d1991b90f9a8539bbdf0f3c20
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-13 15:23:32 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns a string's length
+ * @s: string
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	while (s[i])
+		i++;
+
+	return (i);
+}
