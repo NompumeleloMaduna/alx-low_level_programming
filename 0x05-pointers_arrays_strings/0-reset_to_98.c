675e0392dc4081d00c824fc451ad3d33f3316534
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-13 14:51:39 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes pointer to an integer & updates the value to 98
+ * @n: variabe
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
