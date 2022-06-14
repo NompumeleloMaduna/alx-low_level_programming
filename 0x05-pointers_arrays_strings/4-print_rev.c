da0571216a167c4de2491deca0b04b1755437fa6
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-14 16:01:23 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,26 +9,17 @@
 
 void print_rev(char *s)
 {
-	int i = 0, j = 0;
-	char str [500];
+	int i = 0;
+
 
 	while (*(s + i))
-	{
-		*(str + i) = *(s + i);
 		i ++;
-	}
 	i - i = 1;
 	while (i >= 0)
 	{
-		*(s + i) = *(str + j);
-		j++;
-		i++;
-	}
-	i - i -1;
-	while (i >= 0)
-	{
-		*(s + i) = *(str + j);
-		j++;
+		_putchar(*(s + i));
 		i--:
 	}
+	_putchar('\n');
 }	
+
