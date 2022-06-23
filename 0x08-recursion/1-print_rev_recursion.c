186c5ee33e9dde6ba4fadea7a39682bee352142e
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-23 16:26:56 -0700

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev_recursion - prints string in reverse
+ * @s: string to be printed
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0') 
+	{
+		_putchar(*s);
+		_print_rev_recursion(s - 1);
+	}
+	else 
+		_putchar('\n');
+}
