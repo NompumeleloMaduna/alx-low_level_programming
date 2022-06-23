4dc57e5865e98e944e308ee8bab0dc544f18d14f
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-23 15:54:24 -0700

    loop statements

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,9 +11,8 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		_putchar(*s);
-		_puts_recurion(s + 1);
+		_putchar('\n';
 	}
-	else 
-		_putchar('\n');
-}
-
+		else 
+		_puts_recurion(s + 1);
+}	
