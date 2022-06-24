8a50b545a657692a8f455dc6254880350591c219
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-23 18:11:07 -0700

    Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - returns factorial of a given number
+ * @n: given number or integer
+ * Reurn: factorial
+ */
+
+int factorial(int n)
+{
+	if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+		return (-1);
+}
