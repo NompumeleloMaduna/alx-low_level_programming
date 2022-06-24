5c1fe4770091a0dbacd578530b75e488a7bc8e8c
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-23 17:41:07 -0700

    coe fix

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strlen_recursion: returns length of a string
@@ -10,7 +11,8 @@ int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_strlen_recursion(s + 1);
-		_putchar(*s);
+		return (1 + _strlen_recursion(s + 1));
 	}
+		else
+			return (0);
 }
