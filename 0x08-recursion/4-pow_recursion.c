96fa394ca3dd4296a72553e5856c44696595a109
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-06-23 19:09:42 -0700

    betty

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,16 +12,15 @@ int _pow_recursion(int x, int y)
 
 	if (y == 0)
 	{	return 1;
-	
 	}
 	else if (y < 0)
 	{
-		return -1;
+		return (-1);
 	}
-	else if (y%2 == 0)
+	else if (y % 2 == 0)
 	{
-		return _pow_recursion(x, y/2)*_pow_recursion(x, y/2);
+		return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
 	}
 	else
-		return x*_pow_recursion(x, y/2)*_pow_recursion(x, y/2);
+		return (x * _pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
 }
