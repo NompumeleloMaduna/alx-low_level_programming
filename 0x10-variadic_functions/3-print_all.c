0b98bb64e3ff4b84eec28ee7fc31d44e87bace97
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-07-13 16:04:30 -0700

    added code

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -44,3 +44,45 @@ void print_char(va_list list)
 {
 	printf("%c", va_arg(list, int));
 }
+/**
+ * print_all - Prints anything
+ * @fmt: List of types of arguments passed to the function
+ **/
+void print_all(const char * const fmt, ...)
+{
+	va_list args;
+	int i, j;
+	char *str = "";
+	p_opt ops[] = {
+		{"s", print_string},
+		{"i", print_integer},
+		{"f", print_float},
+		{"c", print_char}
+	};
+
+	va_start(args, fmt);
+
+	i = j = 0;
+	while (fmt != NULL && fmt[i] != 0)
+	{
+
+		while (j < 4)
+		{
+			if (fmt[i] == *(ops[j].op))
+			{
+				printf("%s", str);
+				ops[j].f(args);
+				str = ", ";
+				break;
+			}
+
+			j++;
+		}
+		j = 0;
+
+		i++;
+	}
+
+	printf("\n");
+	va_end(args);
+}
