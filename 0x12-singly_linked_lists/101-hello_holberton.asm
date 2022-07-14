d46caf1bd0ee3060b78a947ec567b98703b9cbb6
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-07-14 15:00:40 -0700

    first commit

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,24 @@
+global main
+
+section .text
+
+main:
+mov rax, 1
+mov rdi, 1
+mov rsi, msg
+mov rdx, msglen
+syscall
+
+mov rax, 60
+mov rdi, 0
+syscall           ;
+
+section .rodata
+msg: db "Hello, Holberton", 10
+msglen: equ $ - msg
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+
