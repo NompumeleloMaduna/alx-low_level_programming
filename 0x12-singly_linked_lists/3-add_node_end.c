fc77d29385aac27f30b48216726e59cd099fe30c
NompumeleloMaduna <nompumelelomaduna@icloud.com>
2022-07-14 14:56:19 -0700

    added code

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -18,4 +18,24 @@ list_t *add_node_end(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
+for (len = 0; str[len] != 0; len++)
+		;
 
+	new->str = malloc(len + 1);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+
+	for (; *head; )
+		head = &(*head)->next;
+
+	*head = new;
+
+	return (new);
+}
